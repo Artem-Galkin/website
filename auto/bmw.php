2531d15b11ba91b5a9572b0f3de2a7db0d859d76
Arteum Galkin <artem.galkin2020@mail.ru>
2022-03-27 20:33:16 +0300

    Инкапсуляция

--- auto/bmw.php
+++ auto/bmw.php
@@ -21,10 +21,67 @@ class Bmw extends Driver
         return "Concern ".  $this->name;
     }  
 
+    private $getClirins;
+    private $baseClirins;
+
+    public function setClirins($clirins)
+    {
+
+        return = $this->getClirins = $clirins;
+    }
+    public function calculateClirins()
+    {
+
+        return = $this->getClirins + 50;
+    }
+    public function getClirins()
+    {
+
+        return $this->$baseClirins;
+    }
+
+
+}
+
+class Img {
+    //2000 строк
+    public function setSrc($pic = "", $w = 20, $h = 10){}
+    public function getResult(){}
+    protected function prepareImg(){}
+    private function reciseImages(){}
+
+
+}
+
+class ImgShape extends Img {
+
+    public function setSrc($pic = "", $w = 20, $h = 10){
+        parent::setSrc(null, $pic);
+        return $this;
+    }
+    public function setZ($z){
+        return $this;
+    }
+    public function getResult(){
+        // логика
+        $this->prepareIng();
+        $this->prepareIng();
+        parent::getResult();
+        // логика
+    }
+
+    $img = new ImgShape();
+    $images = $img->setSrc($img,100,200) -> setZ(40) -> getResalt();
+    
+
+
+
+
+
+
+
 
 
 
 
 }
-
-
