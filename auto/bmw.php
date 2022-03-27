0678230458915299c59ed19d9d285e9f0f3bd195
Arteum Galkin <artem.galkin2020@mail.ru>
2022-03-27 16:25:25 +0300

    Полифармизм

--- auto/bmw.php
+++ auto/bmw.php
@@ -9,11 +9,19 @@ class Bmw extends Driver
 
 
 {
+  
 
     public function __construct($name = "Название не указано")
     {
         $this -> name = $name;
     }
+
+    Public function getName()
+    {
+        return "Concern ".  $this->name;
+    }  
+
+
 
 
 
