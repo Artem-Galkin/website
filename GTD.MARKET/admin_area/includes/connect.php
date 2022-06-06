0d958a7d0bef2dc115d5ef65865ee8596c4e7a84
Arteum Galkin <artem.galkin2020@mail.ru>
2022-06-06 17:53:25 +0300

    подключили SQL базу

---
+++ GTD.MARKET/admin_area/includes/connect.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect('localhost', 'root', '', 'mystore');
+if ($con) {
+  echo "connection successful";
+} else {
+  die(mysqli_error($con));
+};
--- GTD.MARKET/admin_area/index.php
+++ GTD.MARKET/admin_area/index.php
@@ -12,6 +12,9 @@
   <!-- bootstrapt CSS link -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+  <!-- font awesome link-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
   <!-- CSS file link -->
   <link rel="stylesheet" href="../style.css">
@@ -60,41 +63,67 @@
 
     <div class="row">
       <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
-        <div class="px-5">
+        <div class="px-3">
           <a href="#"><img src="../images/face.png" alt="" class="admin_image"></a>
           <p class="text-light text-center">Admin name</p>
         </div>
 
         <!-- button*10>a.nav-link.text-light.bg-info.me-1 -->
         <div class="button text-center">
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_" class="nav-link text-light 
         bg-info my-1">Insert Products</a></button>
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_" class="nav-link text-light 
         bg-info my-1">View Products</a></button>
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_category" class="nav-link text-light 
         bg-info my-1">Insert Categories</a></button>
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_" class="nav-link text-light 
         bg-info my-1">View Categories</a></button>
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_brand" class="nav-link text-light 
         bg-info my-1">Insert Brands</a></button>
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_" class="nav-link text-light 
         bg-info my-1">View Brands</a></button>
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_" class="nav-link text-light 
         bg-info my-1">All orders</a></button>
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_" class="nav-link text-light 
         bg-info my-1">All payments</a></button>
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_" class="nav-link text-light 
         bg-info my-1">List users</a></button>
-          <button class="my-3"><a href="" class="nav-link text-light 
+          <button class="my-3"><a href="index.php?insert_" class="nav-link text-light 
         bg-info my-1">Logout</a></button>
-
         </div>
       </div>
     </div>
 
-    <div class="bg-info p-3 text-center footer">
-      <p>All rights reserved - © Template by Artem Galkin. Все права защищены-2022</p>
+
+
+
+    <!-- fourth child -->
+
+
+    <div class="container my-5">
+
+      <?php
+      if (isset($_GET['insert_category'])) {
+        include('insert_categories.php');
+      }
+
+      if (isset($_GET['insert_brand'])) {
+        include('insert_brands.php');
+      }
+
+
+
+      ?>
     </div>
+
+  </div>
+
+
+
+  <!-- last child -->
+  <div class="bg-info p-3 text-center footer">
+    <p>All rights reserved - © Template by Artem Galkin. Все права защищены-2022</p>
+  </div>
   </div>
 
   <!-- boot strapt JS link -->
