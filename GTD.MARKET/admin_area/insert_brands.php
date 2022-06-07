188ab45812b5490203e6b1969b48efce71bdae1e
Arteum Galkin <artem.galkin2020@mail.ru>
2022-06-07 13:38:18 +0300

    12 работаем с базой данных

--- GTD.MARKET/admin_area/insert_brands.php
+++ GTD.MARKET/admin_area/insert_brands.php
@@ -1,7 +1,24 @@
 <!-- 2) Подключаем к базе данных, проверяем подключение - при запросе должно выдавать "" -->
 <?php
-include('../includes/connect.php')
+include('../includes/connect.php');
+if (isset($_POST['insert_brand'])) {
+  $brand_title = $_POST['brand_title'];
 
+  // select data from data base
+  $select_query = "Select*from `brands` where brand_title='$brand_title'";
+  $result_select = mysqli_query($con, $select_query);
+  $number = mysqli_num_rows($result_select);
+  if ($number > 0) {
+    echo "<script>alert('This Brand is present inside the database')</script>";
+  } else {
+
+    $insert_query = "insert into `brands`(brand_title) values('$brand_title')";
+    $result = mysqli_query($con, $insert_query);
+    if ($result) {
+      echo "<script>alert('Brand has been insered successfully')</script>";
+    }
+  }
+}
 ?>
 
 
