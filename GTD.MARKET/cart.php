856b30684ce3d9994b210fd90e2ce5a0cb5e55f4
Arteum Galkin <artem.galkin2020@mail.ru>
2022-06-25 17:21:53 +0300

    E-commerce website using PHP and MySQL || Displaying empty cart message || Part-33

--- GTD.MARKET/cart.php
+++ GTD.MARKET/cart.php
@@ -2,9 +2,7 @@
 <?php
 include('includes/connect.php');
 include('functions/common_function.php');
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,11 +11,8 @@ include('functions/common_function.php');
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ecommerce Website-Cart details</title>
-
-
   <!-- bootstrapt CSS link -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
   <!-- font awesome link-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="style.css">
@@ -29,7 +24,6 @@ include('functions/common_function.php');
     }
   </style>
 </head>
-
 <!-- css file -->
 <link rel="stylesheet" href="style.css">
 
@@ -60,15 +54,10 @@ include('functions/common_function.php');
         </div>
       </div>
     </nav>
-
     <!-- calling cart function -->
-
-
     <?php
     cart();
     ?>
-
-
     <!-- second child -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
       <ul class="navbar-nav me-auto">
@@ -80,81 +69,55 @@ include('functions/common_function.php');
         </li>
       </ul>
     </nav>
-
-
     <!-- fhird child -->
-
     <div class="bg-light">
       <h3 class="text-center">Hidden Store</h3>
       <p class="text-center">Communication is at the heart of e-commerce and community</p>
     </div>
+    <!-- fourth child-table -->
+    <div class="container">
+      <div class="row">
+        <form action="" method="post">
 
-
-  </div>
-  </div>
-  </div>
-
-
-
-  <!-- fourth child-table -->
-  <div class="container">
-    <div class="row">
-      <form action="" method="post">
+        </form>
         <table class="table table-bordered text-center">
-
+          <thead>
+            <tr>
+              <th>Product Title</th>
+              <th>Product Image</th>
+              <th>Quantity</th>
+              <th>Total price</th>
+              <th>Remove</th>
+              <th colspan="2">Operations</th>
+            </tr>
+          </thead>
           <tbody>
             <!-- php code to display dynamic data-->
             <?php
+            global $con;
             $get_ip_add = getIPAddress();
             $total_price = 0;
             $cart_query = "Select * from `cart_details` where ip_address='$get_ip_add'";
             $result = mysqli_query($con, $cart_query);
-            $result_count = mysqli_num_rows($result);
-            if ($resuit_count > 0) {
-              //       echo"          <thead>
-              //   <tr>
-              //     <th>Product Title</th>
-              //     <th>Product Image</th>
-              //     <th>Quantity</th>
-              //     <th>Total price</th>
-              //     <th>Remove</th>
-              //     <th colspan='2'>Operations</th>
-              //   </tr>
-              // </thead>"
-              // <tbody>";
-
-              while ($row = mysqli_fetch_array($result)) {
-                $product_id = $row['product_id'];
-                $select_products = "Select * from `products` where product_id='$product_id'";
-                $result_products = mysqli_query($con, $select_products);
-                while ($row_product_price = mysqli_fetch_array($result_products)) {
-                  $product_price = array($row_product_price['product_price']);
-                  $price_table = $row_product_price['product_price'];
-                  $product_title = $row_product_price['product_title'];
-                  $product_image1 = $row_product_price['product_image1'];
-                  $product_values = array_sum($product_price);  //[500]
-                  $total_price += $product_values;  //[500]
+            while ($row = mysqli_fetch_array($result)) {
+              $product_id = $row['product_id'];
+              $select_products = "Select * from `products` where product_id='$product_id'";
+              $result_products = mysqli_query($con, $select_products);
+              while ($row_product_price = mysqli_fetch_array($result_products)) {
+                $product_price = array($row_product_price['product_price']);    //[200,300]
+                $price_table = $row_product_price['product_price'];
+                $product_title = $row_product_price['product_title'];
+                $product_image1 = $row_product_price['product_image1'];
+                $product_values = array_sum($product_price);  //[500]
+                $total_price += $product_values;  //[500]
+                echo ""
             ?>
-
-                  <thead>
-                    <tr>
-                      <th>Product Title</th>
-                      <th>Product Image</th>
-                      <th>Quantity</th>
-                      <th>Total price</th>
-                      <th>Remove</th>
-                      <th colspan='2'>Operations</th>
-                    </tr>
-                  </thead>"
-
-          <tbody>
-
-
-            <tr>
-              <td><?php echo $product_title ?></td>
-              <td><img src="./admin_area/product_images/<?php echo $product_image1 ?>" alt="" class="cart_img"></td>
-              <td><input type="text" name="qty" id="" class="form-input w-50"></td>
-              <?php
+                <tr>
+                  <td><?php echo $product_title ?></td>
+                  <td><img src="./admin_area/product_images/<?php echo $product_image1 ?>" alt="" class="cart_img"></td>
+                  <td><input type="text" name="" id="" class="form-input w-50"></td>
+                  <td><input type="text" name="qty" id="" class="form-input w-50"></td>
+                  <?php
                   $get_ip_add = getIPAddress();
                   if (isset($_POST['update_cart'])) {
                     $quantities = $_POST['qty'];
@@ -162,83 +125,39 @@ include('functions/common_function.php');
                     $result_products_quantity = mysqli_query($con, $update_cart); //  перемножает количество со стоимостью  Part -31 (9:30)
                     $total_price = $total_price * $quantities;
                   }
-              ?>
-              <td><?php echo $price_table ?>/-</td>
-              <td><input type="checkbox" name="removeitem[]" value="<?php echo $product_id ?>"></td>
-              <td>
-                <!-- <button class="bg-info px-3 py-2 border-0 mx-3">Update</button> -->
-                <input type="submit" value="Ubdate Cart" class="bg-info px-3 py-2 border-0 mx-3" name="update_cart">
-                <!-- <button class="bg-info px-3 py-2 border-0 mx-3" name="update_cart">Remove</button> -->
-                <input type="submit" value="Remove Cart" class="bg-info px-3 py-2 border-0 mx-3" name="remove_cart">
-              </td>
-            </tr>
-      <?php
-                }
+                  ?>
+                  <td><?php echo $price_table ?>/-</td>
+                  <td><input type="checkbox"></td>
+                  <td>
+                    <button class="bg-info px-3 py-2 border-0 mx-3">Update</button><button class="bg-info px-3 py-2 border-0 mx-3">Remove</button>
+                    <!-- <button class="bg-info px-3 py-2 border-0 mx-3">Update</button> -->
+                    <input type="submit" value="Ubdate Cart">
+                    <button class="bg-info px-3 py-2 border-0 mx-3" name="update_cart">Remove</button>
+                  </td>
+                </tr>
+            <?php
               }
-            } else {
-              echo "<h2 class='text-center text-danger'>Cart is empty</h2>";
-            }
-      ?>
+            } ?>
           </tbody>
         </table>
-
         <!-- subtotal -->
-        <div class="d-flex mb-5">
-
-          <?php
-          $get_ip_add = getIPAddress();
-          $cart_query = "Select * from `cart_details` where ip_address='$get_ip_add'";
-          $result = mysqli_query($con, $cart_query);
-          $result_count = mysqli_num_rows($result);
-          if ($resuit_count > 0) {
-            echo "          <h4 class='px-3'>Subtotal:<strong class='text-info'>$total_price/-</strong>
-          </h4>
-          <input type='submit' value='Continue Shopping' class='bg-info px-3 py-2 border-0 mx-3' name='continue_shopping'>
-          <a href=''><button class='bg-secondary p-3 py-2 border-0 text-light'>Checkout</button></a>";
-          } else {
-            echo " <input type='submit' value='Continue Shopping' class='bg-info px-3 py-2 border-0 mx-3' name='continue_shopping'>";
-          }
-          if (isset($_POST['continue_shopping'])) {
-            echo "<script>window.open('index.php','_self')</script>";
-          }
-          ?>
-
-
+        <div class="d-flex mb-3">
+          <h4 class="px-3">Subtotal:<strong class="text-info"><?php echo $total_price ?>/-</strong></h4>
+          <a href="index.php"><button class="bg-info px-3 py-2 border-0 mx-3">Continue Shopping</button></a>
+          <a href="#"><button class="bg-secondary p-3 py-2 border-0 text-light">Checkout</button></a>
         </div>
+      </div>
     </div>
+
+    </form>
   </div>
-  </form>
-
-
-  <!--function to remove item  -->
-
-  <?php
-  function remove_cart_item()
-  {
-    global $con;
-    if (isset($_POST['remove_cart'])) {
-      foreach ($_POST['removeitem'] as $remove_id) {
-        echo $remove_id;
-        $delete_query = "Delete from `cart_details` where product_id=$remove_id";
-        $run_delete = mysqli_query($con, $delete_query);
-        if ($run_delete) {
-          echo "<script>window.open('cart.php','_self')</>";
-        }
-      }
-    }
-  }
-
-
-  echo $removi_item = remove_cart_item();
-  ?>
-
+  </div>
+  </div>
   <!-- last child -->
   <!-- include footer -->
   <?php include("./includes/footer.php") ?>
-
-  <!-- bootstrapt JS link -->
+  <!-- boot strapt JS link -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
 </body>
 
 </html>
