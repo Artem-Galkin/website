d02b5e4e167ef5161b441f13caed8208a7246249
Arteum Galkin <artem.galkin2020@mail.ru>
2022-06-29 21:49:46 +0300

    E-Commerce Website using PHP and MySQL || User Redirection || Part -44

--- GTD.MARKET/users_area/user_login.php
+++ GTD.MARKET/users_area/user_login.php
@@ -12,6 +12,11 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 
+  <style>
+    body {
+      overflow-x: hidden;
+    }
+  </style>
 
 </head>
 
@@ -21,7 +26,7 @@
     <h2 class="text-center">User Login</h2>
     <div class="row d-flex align-items-center justify-content-center mt-5">
       <div class="col-lg-12 col-xl-6">
-        <form action="" method="post" enctype="multipart/form-data">
+        <form action="" method="post">
 
           <!-- username feield -->
           <div class="form-outline mb-4">
@@ -51,3 +56,23 @@
 </body>
 
 </html>
+
+<?php
+if (isset($_POST['user_login'])) {
+  $user_username = $_POST['user_username'];
+  $user_password = $_POST['user_password'];
+  $select_query = "Select*from `user_table` where username=' $user_username'";
+  $result = mysqli_query($con, $select_products);
+  $row_count = mysqli_num_rows($result);
+  $row_data = mysqli_fetch_assoc($result);
+  if ($row_count > 0) {
+    if (password_verify($user_password, $row_data['user_password'])) {
+      echo "<script>alert('Login successful')</script>";
+    } else {
+      echo "<script>alert('Invalid Credentials')</script>";
+    }
+  } else {
+    echo "<script>alert('Invalid Credentials')</script>";
+  }
+}
+?>
